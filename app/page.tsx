83d8c6f40ee5a6dda55858fb8563fea502d46ec6
benjamin-mahmood <benmahmood@icloud.com>
2025-02-16 13:15:05 +0100

    installed ShadCN

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main>
       <div className='text-8xl text-center font-bold'>Notion AI Clone</div>
+      <Button>Klick Mich!</Button>
     </main>
   )
 }
